ce1cda585ba838bf3aff8afa6f6dc64a66a20bbd
Vicky Villeta <viv226@lehigh.edu>
2024-11-29 19:08:33 -0500

    basic home page done

--- WanderWise/src/app/app.component.ts
+++ WanderWise/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WanderWise';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log('Navigation ended:', event.urlAfterRedirects);
+      }
+    });
+  }
 }
+
