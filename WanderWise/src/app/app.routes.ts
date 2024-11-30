45aba44c740def192c87b3abba667fbacfc82d42
Vicky Villeta <viv226@lehigh.edu>
2024-11-29 23:28:03 -0500

    profile and subscription page created and home updated

--- WanderWise/src/app/app.routes.ts
+++ WanderWise/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SubscribeComponent } from './pages/subscribe/subscribe.component';
 
 export const routes: Routes = [ // Exporting the routes array
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'profile', component: ProfileComponent },
-    { path: 'dashboard', component: DashboardComponent }
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'subscribe', component: SubscribeComponent }
   ];
   
   @NgModule({
