a988c02ad78a6f620aa91b2e7065eda645430d52
Tony <toc326@lehigh.edu>
2024-12-01 20:53:36 -0500

    getting login to show

--- WanderWise/src/app/app.routes.ts
+++ WanderWise/src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SubscribeComponent } from './pages/subscribe/subscribe.component';
+import { LoginComponent } from './pages/login/login.component';
+
 
 export const routes: Routes = [ // Exporting the routes array
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'subscribe', component: SubscribeComponent }
+    { path: 'subscribe', component: SubscribeComponent },
+    { path: 'login', component: LoginComponent }
   ];
   
   @NgModule({
